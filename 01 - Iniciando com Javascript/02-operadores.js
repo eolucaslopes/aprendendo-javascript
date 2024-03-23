95542842b7420ed926e2f162811045ac6aa3ef14
eolucaslopes <eolucaslopes@gmail.com>
2024-03-23 00:14:57 -0300

    feat: adiciona excercícios na aula 02

--- 01 - Iniciando com Javascript/02-operadores.js
+++ 01 - Iniciando com Javascript/02-operadores.js
@@ -1,8 +1,15 @@
-console.log('1 + 1 é:', 1+1) // 1 + 1 é: 2
-console.log('4 - 2 é:', 4-2) // 4 - 2: 2
-console.log('5 * 5 é:', 5*5) // 5 * 5: 25
-console.log('8 / 4 é:', 8/2) // 8 / 4: 2
-console.log('2 % 4 é:', 2%4) // 2 % 4: 2 
-console.log('2 ** 4 é:', 2**4) // 2 ** é:
+console.log('1 + 1 é:', 1 + 1) // 1 + 1 é: 2
+console.log('4 - 2 é:', 4 - 2) // 4 - 2: 2
+console.log('5 * 5 é:', 5 * 5) // 5 * 5: 25
+console.log('8 / 4 é:', 8 / 2) // 8 / 4: 2
+console.log('2 % 4 é:', 2 % 4) // 2 % 4: 2 
+console.log('2 ** 4 é:', 2 ** 4) // 2 ** é: 16
+
+console.log('\n----- Operadores -----\n')
+
+console.log('1 > 2 é:' 1 > 2) // false
+console.log('2 < 4 é:', 2 < 4) // true
+
+
 
 
