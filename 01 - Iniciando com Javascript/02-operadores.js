8e881a63bdca6824f8384140263cd23665f5506f
eolucaslopes <eolucaslopes@gmail.com>
2024-03-24 23:47:54 -0300

    feat: adiciona novos exercícios

--- 01 - Iniciando com Javascript/02-operadores.js
+++ 01 - Iniciando com Javascript/02-operadores.js
@@ -5,10 +5,14 @@ console.log('8 / 4 é:', 8 / 2) // 8 / 4: 2
 console.log('2 % 4 é:', 2 % 4) // 2 % 4: 2 
 console.log('2 ** 4 é:', 2 ** 4) // 2 ** é: 16
 
-console.log('\n----- Operadores -----\n')
+console.log('\n----- Operadores Lógicos -----\n')
 
-console.log('1 > 2 é:' 1 > 2) // false
+console.log('1 > 2 é:', 1 > 2) // false
 console.log('2 < 4 é:', 2 < 4) // true
+console.log('1 === 2 é:', 1 === 2)
+console.log('1 !== 2 é:', 1 !== 2)
+console.log('1 >= 1 é:', 1 >= 1)
+
 
 
 
