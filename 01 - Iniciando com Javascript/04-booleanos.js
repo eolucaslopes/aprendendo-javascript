720d700aff6eec554effbcdb0485208b0591209c
eolucaslopes <eolucaslopes@gmail.com>
2024-03-25 01:01:24 -0300

    adiciona nova aula - booleanos

---
+++ 01 - Iniciando com Javascript/04-booleanos.js
@@ -0,0 +1,21 @@
+// Tipos falso: false, 0, [ ], " ", ' ', undefined, null, NaN
+console.log("----- Tipos falso -----\n")
+
+console.log(Boolean(false));        // false
+console.log(Boolean(0));            // false
+console.log(Boolean([]));           // false com uma resalva, valores vazios são avaliados como false quando convertidos para booleanos em JavaScript.
+console.log(Boolean(' '));          // false com uma resalva, valores vazios são avaliados como false quando convertidos para booleanos em JavaScript.
+console.log(Boolean(" "));          // false com uma resalva, valores vazios são avaliados como false quando convertidos para booleanos em JavaScript.
+console.log(Boolean(undefined));    // false
+console.log(Boolean(null));         // false
+console.log(Boolean(NaN));          // false
+
+
+// Tipos verdadeiros: true, 1, -1, 0.5, "0"
+console.log("\n----- Tipos verdadeiros -----\n")
+
+console.log(Boolean(true));         // true
+console.log(Boolean(1));            // true
+console.log(Boolean(-1));           // true
+console.log(Boolean(0.5));          // true
+console.log(Boolean("0"));          // true
