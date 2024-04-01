52676ac511db79afb9f46bb1818c70452c96955e
eolucaslopes <eolucaslopes@gmail.com>
2024-03-31 21:53:54 -0300

    feat: adiciona aula 06

---
+++ 01 - Iniciando com Javascript/06-trabalhando-com-objetos.js
@@ -0,0 +1,10 @@
+const heroi = {
+    nome: 'Flash',
+    poder: 'Força',
+    poder3: 'Agilidade',
+    poder4: 'Velocidade',
+    poder2: 'Inteligência',
+    poder5: 'Destreza'
+}
+
+console.log(`Olá, eu sou o ${heroi.nome}!`)
